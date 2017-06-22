f4172534e5e29e53850e4d45561413f8f75a6a5e
Hieu Minh <info.hieuit@gmail.com>
2017-06-22 11:39:45 +0700

    create function login signup logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by email: params[:session][:email].downcase
+
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = t ".success_login"
+      log_in user
+      redirect_to root_url
+    else
+      flash.now[:danger] = t ".error_login"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_path
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: :show
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = t ".check_email"
+      redirect_to root_url
+    else
+      flash.now[:danger] = t ".error"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation)
   end
 end
